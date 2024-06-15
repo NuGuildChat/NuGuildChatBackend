c3c35ddc8a4332687dd7e3b5b6e3267381456490
a-moseman <drewmoseman@gmail.com>
2024-06-15 13:16:32 -0400

    Refactor channelUUID to channelID

--- src/main/java/org/amoseman/nuguildchatbackend/pojo/message/MessageRecord.java
+++ src/main/java/org/amoseman/nuguildchatbackend/pojo/message/MessageRecord.java
@@ -5,8 +5,8 @@ public class MessageRecord extends Message {
     private final long created;
     private final long updated;
 
-    public MessageRecord(String authorUUID, String channelUUID, String contents, long id, long created, long updated) {
-        super(authorUUID, channelUUID, contents);
+    public MessageRecord(String authorUUID, long channelID, String contents, long id, long created, long updated) {
+        super(authorUUID, channelID, contents);
         this.id = id;
         this.created = created;
         this.updated = updated;
