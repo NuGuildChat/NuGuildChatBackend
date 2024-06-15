8132bf8ca3f8b9454a3bc0891d52a18e09664741
a-moseman <drewmoseman@gmail.com>
2024-06-15 13:32:17 -0400

    Add shadow plugin

--- src/main/java/org/amoseman/nuguildchatbackend/api/NuGuildChatConfiguration.java
+++ src/main/java/org/amoseman/nuguildchatbackend/api/NuGuildChatConfiguration.java
@@ -1,27 +1,40 @@
 package org.amoseman.nuguildchatbackend.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.core.Configuration;
+import jakarta.validation.constraints.NotEmpty;
 
 public class NuGuildChatConfiguration extends Configuration {
+    @NotEmpty
     private final String databaseURL;
-    private final String databaseUsername;
-    private final String databasePassword;
+    @NotEmpty
+    private String databaseUsername;
+    @NotEmpty
+    private String databasePassword;
 
-    public NuGuildChatConfiguration(String databaseURL, String databaseUsername, String databasePassword) {
-        this.databaseURL = databaseURL;
-        this.databaseUsername = databaseUsername;
-        this.databasePassword = databasePassword;
-    }
+    @JsonProperty
 
     public String getDatabaseURL() {
         return databaseURL;
     }
 
+    @JsonProperty
     public String getDatabaseUsername() {
         return databaseUsername;
     }
 
+    @JsonProperty
+    public void setDatabaseUsername(String databaseUsername) {
+        this.databaseUsername = databaseUsername;
+    }
+
+    @JsonProperty
     public String getDatabasePassword() {
         return databasePassword;
+    }
+
+    @JsonProperty
+    public void setDatabasePassword(String databasePassword) {
+        this.databasePassword = databasePassword;
     }
 }
